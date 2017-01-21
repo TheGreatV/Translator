44fef56c77a3ca1199a58627c70b5fb8b7b8cc1b
Great V <GreatV@email.ua>
2017-01-21 08:18:01 +0200

    [Update] Console

--- Source/Console/Source.cpp
+++ Source/Console/Source.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <fstream>
+
+#include <Translator/Lexer.hpp>
+using namespace Translator;
+
 
 void main()
 {
+	Lexer::Glyphs source;
+	{
+		std::ifstream file("../../../../Media/Source.e"); // relative to project (.vcxproj)
+		std::getline(file, source, '\0');
+		file.close();
+	}
+
+	Lexer lexer;
+	auto tokens = Move(lexer.Parse(source));
+
+
 	std::system("pause");
 }
